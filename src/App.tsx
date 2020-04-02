55060b4e5fc651d81902d5dd3d7fa5fdb1382f7e
Jeremy Levett <jlevett@engineer.com>
2020-04-02 19:49:47 +1100

    fix react router pwa

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,14 @@ import {
 import * as styles from './app.css';
 import RightNow from './components/RightNow';
 import Graph from './components/Graph';
-import PageNotFound from './components/PageNotFound';
 
 const App = () => {
   return(
     <div className="App">
       <Router>
         <Switch> 
-          <Route path="/graph" component={Graph} />
+          <Route exact path="/graph" component={Graph} />
           <Route path="/" component={RightNow} />
-         
-          <Route component={PageNotFound} />  
         </Switch>  
         <div css={styles.fixed}>
           <Link to="/"><button  css={styles.left}>Current Status</button></Link>
